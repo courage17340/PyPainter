615d4e3e84cea3641e88d95dc08d855794cb8401
courage17340 <courage17340@163.com>
2015-05-20 15:14:49 +0800

    v0.1

--- PyPainter.py
+++ PyPainter.py
@@ -3,6 +3,8 @@ import os
 root = Tk()
 image = "1.bmp"
 canv = 0
+table = [[16777215 for col in range(802)] for row in range(602)]
+
 
 def nop():
     print "nothing happened"
@@ -20,7 +22,6 @@ def put_pixel(x, y, r, g, b):
 
 def open_image():
     global image
-    head = []
     if not os.path.exists(image):
         print "No such file"
         return
@@ -30,10 +31,72 @@ def open_image():
         print "error"
         img.close()
         return
-    length = ord(head[18]) + ord(head[19]) * 256
-    width = ord(head[22]) + ord(head[23]) * 256
-    print length, width
+    width = ord(head[18]) + ord(head[19]) * 256
+    height = ord(head[22]) + ord(head[23]) * 256
+    t = width * 3 / 4 * 4
+    if t < width * 3:
+        t += 4
+    t -= width * 3
+    #global table
+    for j in range(height, 0, -1):
+        for i in range(1, width + 1):
+            b = ord(img.read(1))
+            g = ord(img.read(1))
+            r = ord(img.read(1))
+            put_pixel(i, j, r, g, b)
+            table[j][i] = (r << 16) + (g << 8) + b
+        for i in range(t):
+            empty = img.read(1)
     img.close()
+
+def make_str(x, num):
+    s = ""
+    for i in range(num):
+        s += chr(x & 255)
+        x >>= 8
+    return s
+
+def write_image():
+    img = open("2.bmp", "wb")
+    global canv
+    width = canv.winfo_width() - 4
+    height = canv.winfo_height() - 4
+    t = width * 3 / 4 * 4
+    if t < width * 3:
+        t += 4
+    size = 54 + t * height
+    string = ""
+    #===header===
+    string += "BM"
+    string += make_str(size, 4)
+    string += make_str(0, 2)
+    string += make_str(0, 2)
+    string += make_str(54, 4)
+    #===info header===
+    string += make_str(40, 4)
+    string += make_str(width, 4)
+    string += make_str(height, 4)
+    string += make_str(1, 2)
+    string += make_str(24, 2)
+    string += make_str(0, 4)
+    string += make_str(size - 54, 4)
+    string += make_str(3780, 4)
+    string += make_str(3780, 4)
+    string += make_str(0, 4)
+    string += make_str(0, 4)
+    #===data====
+    #global table
+    t -= width * 3
+    for j in range(height, 0, -1):
+        for i in range(1, width + 1):
+            color = table[j][i]
+            string += make_str(color, 3)
+        for i in range(t):
+            string += make_str(0, 1)
+    #===end===
+    img.write(string)
+    img.close()
+    
 
 def pack_menu_list():
     global root
@@ -44,7 +107,7 @@ def pack_menu_list():
     m.add_cascade(label = "File", menu = filemenu)
     filemenu.add_command(label = "New", command = nop)
     filemenu.add_command(label = "Open...", command = open_image)
-    filemenu.add_command(label = "Save As...", command = nop)
+    filemenu.add_command(label = "Save As...", command = write_image)
     filemenu.add_separator()
     filemenu.add_command(label = "Exit", command = end)
     #===help===
@@ -58,12 +121,12 @@ def pack_menu_list():
 def pack_tool_list():
     global root
     f = Frame(root, width = 100, height = 600)
-    f.pack(side="left")
+    f.pack(side = "left")
     return f
 
 def pack_main_table():
-    c = Canvas(root,width=800,height=600,bg="white")
-    c.pack(side="left")
+    c = Canvas(root,width = 800,height = 600,bg = "white")
+    c.pack(side = "left")
     return c
 
 def main():
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# PyPainter
+#PyPainter
 
-My python project for programming course
+##Brief introduction
+
+Chu Wei's python project
+
+##Version details
+| Version | Date | Description |
+|:-------:|:----:|:-----------:|
+| 0.1 | 2015.5.20 | load & save bmp files |
