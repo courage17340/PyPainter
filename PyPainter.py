79c88d16ed84108326d30bd30b272f039e0352f2
courage17340 <courage17340@163.com>
2015-06-08 19:58:58 +0800

    backup

--- PyPainter.py
+++ PyPainter.py
@@ -4,7 +4,16 @@ root = Tk()
 image = "1.bmp"
 canv = 0
 table = [[16777215 for col in range(802)] for row in range(602)]
-
+v = IntVar()
+v.set(1)
+rv = StringVar()
+rv.set("0")
+gv = StringVar()
+gv.set("0")
+bv = StringVar()
+bv.set("0")
+hx = []
+hy = []
 
 def nop():
     print "nothing happened"
@@ -15,6 +24,13 @@ def end():
 
 def rgb(r, g, b):
     return "#%02x%02x%02x" % (r, g, b)
+
+def get_rgb():
+    global rv, gv, bv
+    r = eval(rv.get())
+    g = eval(gv.get())
+    b = eval(bv.get())
+    return rgb(r, g, b)
 
 def put_pixel(x, y, r, g, b):
     global canv
@@ -129,12 +145,57 @@ def pack_main_table():
     c.pack(side = "left")
     return c
 
+def clear_screen():
+    global canv, root, table
+    canv.destroy()
+    canv = pack_main_table()
+    table = [[16777215 for col in range(802)] for row in range(602)]
+    canv.bind("<Button-1>", paint_start)
+    canv.bind("<B1-Motion>", paint_work)
+    canv.bind("<ButtonRelease-1>", paint_end)
+    canv.bind("<Double-Button-1>", poly_end)
+
+def paint_start(event):
+    print "--start--"
+
+def paint_work(event):
+    print "--work--"
+
+def paint_end(event):
+    print "--end--"
+
+def poly_end(event):
+    print "--poly--"
+
 def main():
     global root
     menu = pack_menu_list()
     tool = pack_tool_list()
+    global v, rv, gv, bv
+    color = Frame(tool, width = 90, height = 100, bd = 4, relief = "groove")
+    color.pack()
+    Label(color, text = "red value:").pack()
+    Entry(color, textvariable = rv).pack()
+    Label(color, text = "green value:").pack()
+    Entry(color, textvariable = gv).pack()
+    Label(color, text = "blue value:").pack()
+    Entry(color, textvariable = bv).pack()
     global canv
     canv = pack_main_table()
+    model = Frame(tool, width = 90, height = 100, bd = 4, relief = "groove")
+    model.pack()
+    Radiobutton(model, text = "Pencil", variable = v, value = 1).grid(sticky = W)
+    Radiobutton(model, text = "Line", variable = v, value = 2).grid(sticky = W)
+    Radiobutton(model, text = "Polygon", variable = v, value = 3).grid(sticky = W)
+    Radiobutton(model, text = "Oval", variable = v, value = 4).grid(sticky = W)
+    Radiobutton(model, text = "Rectangle", variable = v, value = 5).grid(sticky = W)
+    Button(tool, text = "Clear", command = clear_screen).pack()
+    Button(tool, text = "Quit", command = end).pack()
+    tool.update()
+    canv.bind("<Button-1>", paint_start)
+    canv.bind("<B1-Motion>", paint_work)
+    canv.bind("<ButtonRelease-1>", paint_end)
+    canv.bind("<Double-Button-1>", poly_end)
     root.mainloop()
 
 main()
